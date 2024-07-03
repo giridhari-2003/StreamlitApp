68b1e429f876cd8f53aea6d965e5fbfc92762186
Giri <giridhari.byteiq@gmail.com>
2024-07-03 23:06:17 +0530

    1st time

--- protfolio_website.py
+++ protfolio_website.py
@@ -1,7 +1,25 @@
 import streamlit as st
+col1,col2 = st.columns(2)
+background_css = """
+<style>
+body {
+    background-color: #00ff00;
+}
+</style>
+"""
 
+# Apply the custom CSS
+st.markdown(background_css, unsafe_allow_html=True)
 
-st.subheader('Hi :wave:')
-st.title("I am Giri")
+with col1:
+    st.subheader('Hi :wave:')
+    st.title("I am Giri")
+# with col2:
+    # st.image("Giri.jpg")
 
-st.image("")
+st.title(" ")
+
+st.title("Giri's AI Bot")
+st.write("Ask anything about me")
+st.text_input("")
+st.button("ASK" , use_container_width= 400)
