e059b45f14b8a901f36e40316cf39b80e4e80f74
Giri <giridhari.byteiq@gmail.com>
2024-07-03 23:14:44 +0530

    1st time

--- protfolio_website.py
+++ protfolio_website.py
@@ -23,3 +23,15 @@ st.title("Giri's AI Bot")
 st.write("Ask anything about me")
 st.text_input("")
 st.button("ASK" , use_container_width= 400)
+
+col1, col2 = st.columns(2)
+st.title(" ")
+with col1:
+    st.subheader("Youtube Channel")
+    st.write("- This is a largest CV chanel")
+    st.write("- 400k+ Subscriber")
+    st.write("- Over 150 free Tutorial")
+    st.write("- 1.5 Million Hours+ watch time")
+
+with col2:
+    st.video('https://www.youtube.com/watch?v=_2UqdX8dcsU')
